9bf2495b8ed5f69bfeac57d2fb11b01ca621a561
quinkirsten <quinkirsten@gmail.com>
2024-12-05 17:55:55 -0500

    installed chartjs dependency

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -2,24 +2,23 @@ import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 
 import MenuContainer from "./MenuContainer";
-import Overview from "../components/Overview"
-import ErrorRate from "../components/ErrorRate"
-import Metrics from "../components/Metrics"
-import PodGrid from "../components/PodGrid"
-import RequestLimit from "../components/RequestLimit"
+import Overview from "../components/Overview";
+import ErrorRate from "../components/ErrorRate";
+import Metrics from "../components/Metrics";
+import PodGrid from "../components/PodGrid";
+import RequestLimit from "../components/RequestLimit";
 
-  // Pod level data to be displayed, updates when user clicks into pod
-  // const [podData, setPodData] = useState({});
-  
+// Pod level data to be displayed, updates when user clicks into pod
+// const [podData, setPodData] = useState({});
 
 const MainContainer = ({ username }) => {
-  const url = 'http:/localhost:3000/';
-  
+  const url = "http:/localhost:3000/";
+
   // State for when the menu button is clicked
   const [menu, setMenu] = useState(false);
-  
+
   // Default metric set to latency
-  const [metric, setMetric] = useState('latency');
+  const [metric, setMetric] = useState("latency");
 
   // Determines if the graphs display node data or pod specific data
   const [defaultView, setDefaultView] = useState(true);
@@ -33,40 +32,39 @@ const MainContainer = ({ username }) => {
   //helper function
   const fetchData = async (method, endpoint, body) => {
     try {
-        const response = await fetch(url + endpoint, {
-          method: method,
-          header: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body)
-        });
-        
-        if (response.ok) {
-          const data = await response.json();
-          setOverviewData(data);
-        } else {
-          const data = await response.json();
-          console.error(data);
-          alert(response.statusText);
-        }
-      } catch (error) {
-        console.error(error);
-        alert('Could not fetch data from the server')
+      const response = await fetch(url + endpoint, {
+        method: method,
+        header: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setOverviewData(data);
+      } else {
+        const data = await response.json();
+        console.error(data);
+        alert(response.statusText);
       }
-  }
+    } catch (error) {
+      console.error(error);
+      alert("Could not fetch data from the server");
+    }
+  };
   //Used to populate overview component
   useEffect(() => {
     const body = {
-      type: 'cpu',
-      time: '1d',
-      aggregation: 'avg',
-      level: 'node'
-    }
-    fetchData('POST', 'query', body)
-   const intervalID = setInterval(fetchData, 30000);
+      type: "cpu",
+      time: "1d",
+      aggregation: "avg",
+      level: "node",
+    };
+    fetchData("POST", "query", body);
+    const intervalID = setInterval(fetchData, 30000);
     return () => {
       clearInterval(intervalID);
-    }
-  }, [])
-  
+    };
+  }, []);
 
   return (
     <div>
@@ -74,11 +72,15 @@ const MainContainer = ({ username }) => {
       {!menu && <MenuContainer />}
       <h1>{`Welcome, ${username}`}</h1>
       <div /*grid*/>
-        <Overview overviewData={overviewData}/>
+        <Overview overviewData={overviewData} />
         <RequestLimit defaultView={defaultView} clickedPod={clickedPod} />
         <ErrorRate defaultView={defaultView} clickedPod={clickedPod} />
         <Metrics defaultView={defaultView} clickedPod={clickedPod} />
-        <PodGrid defaultView={defaultView} setDefaultView={setDefaultView} setClickedPod={setClickedPod}/>
+        <PodGrid
+          defaultView={defaultView}
+          setDefaultView={setDefaultView}
+          setClickedPod={setClickedPod}
+        />
       </div>
       <button onClick={() => setDefaultView(true)}>Reset to default</button>
       <button>Ask AI</button>
