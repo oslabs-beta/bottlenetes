633d920a45699bd16fb3f4a451a39478ff9a6bca
quinkirsten <quinkirsten@gmail.com>
2024-12-05 17:53:19 -0500

    set up basic state and components

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MenuContainer from "./MenuContainer";
 import Overview from "../components/Overview"
@@ -8,26 +8,37 @@ import Metrics from "../components/Metrics"
 import PodGrid from "../components/PodGrid"
 import RequestLimit from "../components/RequestLimit"
 
+  // Pod level data to be displayed, updates when user clicks into pod
+  // const [podData, setPodData] = useState({});
+  
 
 const MainContainer = ({ username }) => {
-  const url = 'http:/localhost:3000';
+  const url = 'http:/localhost:3000/';
   
   // State for when the menu button is clicked
   const [menu, setMenu] = useState(false);
-  // 
+  
+  // Default metric set to latency
   const [metric, setMetric] = useState('latency');
+
   // Determines if the graphs display node data or pod specific data
   const [defaultView, setDefaultView] = useState(true);
-  // Fleet wide data that is collected live
+
+  // Overview data to be displayed at the very top
   const [overviewData, setOverviewData] = useState({});
-  // Pod level data to be displayed, updates when user clicks into pod
-  const [podData, setPodData] = useState({})
 
+  // Which pod has been clicked
+  const [clickedPod, setClickedPod] = useState("");
 
-  useEffect(() => {
-    const fetchPodData = async () => {
-      try {
-        const response = await fetch(/*url*/);
+  //helper function
+  const fetchData = async (method, endpoint, body) => {
+    try {
+        const response = await fetch(url + endpoint, {
+          method: method,
+          header: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body)
+        });
+        
         if (response.ok) {
           const data = await response.json();
           setOverviewData(data);
@@ -40,8 +51,20 @@ const MainContainer = ({ username }) => {
         console.error(error);
         alert('Could not fetch data from the server')
       }
+  }
+  //Used to populate overview component
+  useEffect(() => {
+    const body = {
+      type: 'cpu',
+      time: '1d',
+      aggregation: 'avg',
+      level: 'node'
     }
-    setInterval(fetchPodData, 30000);
+    fetchData('POST', 'query', body)
+   const intervalID = setInterval(fetchData, 30000);
+    return () => {
+      clearInterval(intervalID);
+    }
   }, [])
   
 
@@ -51,15 +74,15 @@ const MainContainer = ({ username }) => {
       {!menu && <MenuContainer />}
       <h1>{`Welcome, ${username}`}</h1>
       <div /*grid*/>
-        <Overview />
-        <RequestLimit />
-        <ErrorRate />
-        <Metrics />
-        <PodGrid />
+        <Overview overviewData={overviewData}/>
+        <RequestLimit defaultView={defaultView} clickedPod={clickedPod} />
+        <ErrorRate defaultView={defaultView} clickedPod={clickedPod} />
+        <Metrics defaultView={defaultView} clickedPod={clickedPod} />
+        <PodGrid defaultView={defaultView} setDefaultView={setDefaultView} setClickedPod={setClickedPod}/>
       </div>
-      <button>Reset to default</button>
+      <button onClick={() => setDefaultView(true)}>Reset to default</button>
       <button>Ask AI</button>
-   ;</div>
+    </div>
   );
 };
 
