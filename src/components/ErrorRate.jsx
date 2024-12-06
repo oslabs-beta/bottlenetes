2abf6207fe14b96748b4fd6497ee3fc7162a448b
Mark <mark.kietnghiem@gmail.com>
2024-12-06 14:00:13 -0800

    feat: Adding TestGrid.jsx for testing purposes

--- src/components/ErrorRate.jsx
+++ src/components/ErrorRate.jsx
@@ -1,1 +1,9 @@
-import React from 'react';
+
+
+const ErrorRate = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default ErrorRate;
---
+++ src/components/Metrics.jsx
@@ -0,0 +1,7 @@
+const Metrics = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default Metrics;
