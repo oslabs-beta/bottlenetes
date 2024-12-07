46d3161e402c8798934ed9935280862261f98baf
Mark <mark.kietnghiem@gmail.com>
2024-12-06 17:22:59 -0800

    Completed RequestLimit.jsx with fake data. Added test-components folder for test components

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import TestGrid from './components/TestGrid'
+// import TestGrid from './test-components/TestGrid'
+// import TestRequestLimit from './test-components/TestRequestLimit'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    {/* <TestGrid /> */}
   </StrictMode>,
 )
