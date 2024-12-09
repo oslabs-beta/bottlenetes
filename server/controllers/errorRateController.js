80bda7c8c05e869c08c204435622668e0611783a
Zoe X <zoexu02@gmail.com>
2024-12-09 07:56:43 -0800

    weekend updates

--- server/controllers/errorRateController.js
+++ server/controllers/errorRateController.js
@@ -1,19 +1,23 @@
 import fetch from "node-fetch";
 import moment from "moment";
+
 export const generateErrorQuery = (req, res, next) => {
-  console.log("We're in generate error query middleware: ", req.body);
-  const { query } = req.body;
-  console.log(query);
-  res.locals.queryString = "";
+  // console.log("We're in generate error query middleware: ", req.body);
+  const query = req.body;
   const queriesArr = {};
   const endTime = moment().toISOString();
   const startTime = moment().subtract(12, "hours").toISOString();
-  for (const key in query) {
+  // console.log(query)
+  // console.log("about to enter for loop")
+  for (let key in query) {
     queriesArr[key] = `http://localhost:9090/api/v1/query_range?query=${encodeURIComponent(query[key])}&start=${encodeURIComponent(startTime)}&end=${encodeURIComponent(endTime)}&step=3600s`;
   }
+
+  // console.log("exited for loop")
   res.locals.errorRateQueries=queriesArr;
-  // console.log(res.locals.errorRateQueries)
+  console.log(res.locals.errorRateQueries)
   return next()
 };
+
 
 export const queryForErrors = async (req, res, next) => {};
